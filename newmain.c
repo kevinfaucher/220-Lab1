66f7e493818eb72548692660ebc9c4c8bded762d
Kevin <Kevin@DESKTOP-J1KBRTK.T-mobile.com>
2017-02-13 23:13:05 -0500

    Git Update

---
+++ newmain.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   newmain.c
+ * Author: Kevin
+ *
+ * Created on February 13, 2017, 11:12 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
